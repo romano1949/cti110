4b3619f4f84ccbf0aa98976e46350d696632e774
romano1949 <54993384+romano1949@users.noreply.github.com>
2019-09-06 11:50:23 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_OmarRoman.py
@@ -0,0 +1,14 @@
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit =total_sales * 0.23
+
+# Display the profit
+print('The profit is $', format(profit, ',.2f'))
+
+# We are learing how to put sales into Python
+# 8/28/2019
+# CTI-110 P2T1- Sales Prediction
+# Omar Roman Jr
+#
